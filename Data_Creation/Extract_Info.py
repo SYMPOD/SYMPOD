e9e064d248a38653fb5190e0d0b98638e343dbd9
sergio <si.rincon@uniandes.edu.co>
2024-11-14 18:30:56 -0500

    Update Data_Creation

--- Data_Creation/Extract_Info.py
+++ Data_Creation/Extract_Info.py
@@ -5,9 +5,12 @@ from tqdm import tqdm
 from Data_Utils import extract_ID, get_information, append_crystal_info, save_powder_image
 
 warnings.filterwarnings("ignore")
-os.mkdir(os.path.join('Data_Creation','Data'))
-os.mkdir(os.path.join('Data_Creation','Data','Structures'))
-os.mkdir(os.path.join('Data_Creation','Data','Powder_images'))
+if os.path.exists(os.path.join('Data_Creation', 'Data')):
+    pass
+else:
+    os.mkdir(os.path.join('Data_Creation','Data'))
+    os.mkdir(os.path.join('Data_Creation','Data','Structures'))
+    os.mkdir(os.path.join('Data_Creation','Data','Powder_images'))
 ls = glob.glob(os.path.join('Data_Creation','Files','*.cif'), recursive = True)
 for i in tqdm(range(len(ls))):
     ID = extract_ID(ls[i])
